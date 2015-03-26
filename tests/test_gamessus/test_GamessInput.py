588b9bdc32783375646bb1abb8c31d87db93ce9d
Lukasz Mentel <lmmentel@gmail.com>
2015-03-26 14:19:52 +0100

    changed the name of the GamessInput tests and correctd the test to have proper imports and method references

--- tests/test_gamessus/test_GamessInput.py
+++ tests/test_gamessus/test_GamessInput.py
@@ -1,17 +1,17 @@
 import os
 import unittest
 
-from chemtools.gamessus import GamessInpParser
+from chemtools.gamessus import GamessInput 
 
 class TestGIPonHeH2(unittest.TestCase):
 
     def setUp(self):
         inpfile = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/heh2/he-h2_avdz_ormas.inp')
-        self.gip = GamessInpParser()
-        self.inpd = self.gip.parse(inpfile)
+        self.gip = GamessInput(fname=inpfile)
+        self.gip.parse()
 
     def test_inpdict(self):
-        self.assertIsInstance(self.inpd, dict)
+        self.assertIsInstance(self.gip.inpdata, dict)
 
     def test_contrl(self):
         d ={"scftyp" : "rhf",
@@ -23,45 +23,45 @@ class TestGIPonHeH2(unittest.TestCase):
             "itol" : "30",
             "ispher" : "1",
             "units" : "bohr"}
-        self.assertDictEqual(d, self.inpd["$contrl"])
+        self.assertDictEqual(d, self.gip.inpdata["$contrl"])
 
     def test_trans(self):
         d = {"cuttrf" : "1.0d-10"}
-        self.assertDictEqual(d, self.inpd["$trans"])
+        self.assertDictEqual(d, self.gip.inpdata["$trans"])
 
     def test_system(self):
         d = {"timlim" : "525600",
              "mwords" : "100"}
-        self.assertDictEqual(d, self.inpd["$system"])
+        self.assertDictEqual(d, self.gip.inpdata["$system"])
 
     def test_title(self):
-        self.assertEqual(self.inpd["$data"]["title"], "He-H2 FCI")
+        self.assertEqual(self.gip.inpdata["$data"]["title"], "He-H2 FCI")
 
     def test_group(self):
-        self.assertEqual(self.inpd["$data"]["group"], "cnv 2")
+        self.assertEqual(self.gip.inpdata["$data"]["group"], "cnv 2")
 
     def test_atoms(self):
-        self.assertEqual(len(self.inpd["$data"]["atoms"]), 2)
+        self.assertEqual(len(self.gip.inpdata["$data"]["atoms"]), 2)
 
     def  test_atom1_xyz(self):
         t = tuple([0.0, 0.0, -3.0])
-        self.assertTupleEqual(t, self.inpd["$data"]["atoms"][0]["xyz"])
+        self.assertTupleEqual(t, self.gip.inpdata["$data"]["atoms"][0]["xyz"])
 
     def  test_atom1_atomic(self):
-        self.assertEqual(self.inpd["$data"]["atoms"][0]["atomic"], "2.0")
+        self.assertEqual(self.gip.inpdata["$data"]["atoms"][0]["atomic"], "2.0")
 
     def  test_atom1_label(self):
-        self.assertEqual(self.inpd["$data"]["atoms"][0]["label"], "He")
+        self.assertEqual(self.gip.inpdata["$data"]["atoms"][0]["label"], "He")
 
     def  test_atom2_xyz(self):
         t = tuple([0.0, 0.724368, 3.0])
-        self.assertTupleEqual(t, self.inpd["$data"]["atoms"][1]["xyz"])
+        self.assertTupleEqual(t, self.gip.inpdata["$data"]["atoms"][1]["xyz"])
 
     def  test_atom2_atomic(self):
-        self.assertEqual(self.inpd["$data"]["atoms"][1]["atomic"], "1.0")
+        self.assertEqual(self.gip.inpdata["$data"]["atoms"][1]["atomic"], "1.0")
 
     def  test_atom2_label(self):
-        self.assertEqual(self.inpd["$data"]["atoms"][1]["label"], "H")
+        self.assertEqual(self.gip.inpdata["$data"]["atoms"][1]["label"], "H")
 
 class TestGIPonNe(unittest.TestCase):
     ne_no_inp = ''' $CONTRL
@@ -146,14 +146,14 @@ NE 10.0       0.00000000       0.00000000      0.0000000
  $END'''
 
     def setUp(self):
-        self.gip = GamessInpParser()
-        self.inpd = self.gip.parse_from_string(TestGIPonNe.ne_no_inp)
+        self.gip = GamessInput()
+        self.gip.parse_from_string(TestGIPonNe.ne_no_inp)
 
     def test_inpdict(self):
-        self.assertIsInstance(self.inpd, dict)
+        self.assertIsInstance(self.gip.inpdata, dict)
 
     def test_vec(self):
-        self.assertIsInstance(self.inpd["$vec"], str)
+        self.assertIsInstance(self.gip.inpdata["$vec"], str)
 
 if __name__ == "__main__":
     unittest.main()
