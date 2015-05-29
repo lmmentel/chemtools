27f2d5bd95e0b82d8e596862a7a4eb43e2d11224
Lukasz Mentel <lmmentel@gmail.com>
2015-05-29 22:58:08 +0200

    updated the conf.py for documentation to help remove the import errors on building on readthedocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,6 +14,17 @@
 
 import sys
 import os
+# in python >= 3.3
+#from unittest.mock import MagicMock
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['argparse', 'numpy', 'scipy', 'pandas', 'sqlalchemy']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 sys.path.insert(0, '/home/lmentel/Devel/chemtools')
 
