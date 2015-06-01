e9f7838e3ee7dece311839bffc2e7274a82f8c8d
Lukasz Mentel <lmmentel@gmail.com>
2015-06-01 20:35:17 +0200

    added the Katron-Matrin three parameter extrapolation formula and corrected some of the docstrings in cbs

--- chemtools/cbs.py
+++ chemtools/cbs.py
@@ -6,7 +6,18 @@ import numpy as np
 def uste(x, e_cbs, A3, method="CI"):
 
     '''CBS extrapolation using USTE shceme based on
-    A. J. C. Varandas, JPCA 114, 8505-8516 (2010).'''
+    A. J. C. Varandas, JPCA 114, 8505-8516 (2010).
+
+    Args:
+      x : int
+        Cardinal number of the basis set
+      e_cbs : float
+        Approximation to the energy value at the CBS limit
+      A3 : float
+        Empirical parameter
+      method : str
+        One of: *CI*, *CC*
+    '''
 
     # parameters calibrated for MRCI(Q)
     ci_params = {
@@ -32,7 +43,7 @@ def uste(x, e_cbs, A3, method="CI"):
 
     return v
 
-def km(x, e_cbs, a):
+def km2(x, e_cbs, a):
     '''
     Two-point formula for extrapolating the HF reference energy, as proposed by
     A. Karton and J. M. L. Martin, Theor. Chem. Acc. 115, 330.  (2006)
@@ -50,6 +61,26 @@ def km(x, e_cbs, a):
 
     return e_cbs + a* (x + 1)*np.exp(-9.0*np.sqrt(x))
 
+def km3(x, e_cbs, a, b):
+    '''
+    Three-point formula for extrapolating the HF reference energy, as proposed
+    by A. Karton and J. M. L. Martin, Theor. Chem. Acc. 115, 330.  (2006)
+
+    :math:`E^{HF}(X) = E_{CBS} + A\cdot \exp(-B\sqrt{X})`
+
+    Args:
+      x : int
+        Cardinal number of the basis set
+      e_cbs : float
+        Approximation to the energy value at the CBS limit
+      a : float
+        Pre-exponential empirical parameter
+      b : float
+        Exponential empirical parameter
+    '''
+
+    return e_cbs + a*np.exp(-b*np.sqrt(x))
+
 def helgaker_X3(x, a, b):
 
     '''Inverse power extrapolation based on T. Helgaker et. al. JCP 106(23),
@@ -57,26 +88,39 @@ def helgaker_X3(x, a, b):
 
     return a + b * np.power(x,-3)
 
-def cbs_helgaker(x, Dinf, alpha, beta):
+def cbs_helgaker(x, e_cbs, a, b):
 
-    '''Helgaker type extrapolationn to CBS with 3 parameters.'''
+    '''Helgaker type extrapolationn to CBS with 3 parameters.
 
-    return Dinf + alpha*np.power(x, -beta)
+    :math:`E^{HF}(X) = E_{CBS} + A\cdot X^{-B}`
 
-def scf_exp(x, e_cbs, b, a):
+   Args:
+       x : int
+        Cardinal number of the basis set
+      e_cbs : float
+        Approximation to the energy value at the CBS limit
+      b : float
+        Polynomial coefficient
+      a : float
+        Order of the polynomial
+    '''
+
+    return e_cbs + a*np.power(x, -b)
+
+def scf_exp(x, e_cbs, a, b):
     '''
     CBS extrapolation formula by exponential Dunning-Feller relation.
 
-    :math:`E^{HF}(X) = E_{CBS} + b\cdot\exp(-aX)`
+    :math:`E^{HF}(X) = E_{CBS} + A\cdot\exp(-BX)`
 
     Args:
       x : int
         Cardinal number of the basis set
       e_cbs : float
         Approximation to the energy value at the CBS limit
-      b : float
+      a : float
         Pre-exponential empirical parameter
-      c : float
+      b : float
         Exponential empirical parameter
     '''
 
