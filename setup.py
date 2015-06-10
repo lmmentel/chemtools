80ad73afc317582e9a521a3992f737baf7d65a3a
Lukasz Mentel <lmmentel@gmail.com>
2015-06-10 02:33:00 +0200

    added a configuration file for tox based test extended the setup to support sphinx_build and added setup.cfg with build parameters

--- setup.py
+++ setup.py
@@ -1,5 +1,30 @@
 ''' Chemtools '''
 
+from setuptools.command.test import test as TestCommand
+import sys
+
+class Tox(TestCommand):
+
+    #user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
+
+
 from setuptools import setup
 
 def readme():
@@ -40,4 +65,6 @@ setup(
         'Topic :: Scientific/Engineering :: Physics',
     ],
     keywords = 'basis set optimization quantum chemistry molecular physics',
+    tests_require=['tox', 'pytest'],
+    cmdclass = {'test': Tox},
 )
