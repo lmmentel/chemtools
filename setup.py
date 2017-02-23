bcd09f264acc68f5fc0b7150e0319cd24b057f66
Lukasz Mentel <lmmentel@gmail.com>
2017-02-23 23:48:18 +0100

    updated requirements and pep8 style

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 ''' Chemtools setup script'''
 
+from setuptools import setup
 from setuptools.command.test import test as TestCommand
 import sys
 
+
 class Tox(TestCommand):
 
-    #user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+    # user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
 
     def initialize_options(self):
         TestCommand.initialize_options(self)
         self.tox_args = None
+
     def finalize_options(self):
         TestCommand.finalize_options(self)
         self.test_args = []
         self.test_suite = True
+
     def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
+        # import here, cause outside the eggs aren't loaded
         import tox
         import shlex
         args = self.tox_args
@@ -25,52 +29,51 @@ class Tox(TestCommand):
         sys.exit(errno)
 
 
-from setuptools import setup
-
 def readme():
-    '''Return the contents of the README.rst file.'''
+    'Return the contents of the README.rst file.'
     with open('README.rst') as freadme:
         return freadme.read()
 
+
 setup(
-    author = "Lukasz Mentel",
-    author_email = "lmmentel@gmail.com",
-    description = "Python tools for quantum chemical calculations",
-    include_package_data = True,
-    install_requires = [
+    author="Lukasz Mentel",
+    author_email="lmmentel@gmail.com",
+    description="Python tools for quantum chemical calculations",
+    include_package_data=True,
+    install_requires=[
         'sqlalchemy',
         'scipy >= 0.11',
         'numpy >= 1.7',
         'mendeleev',
-    ],
-    entry_points = {
-        'console_scripts' : [
+        'numba',
+        'pandas'],
+    entry_points={
+        'console_scripts': [
             'writeorbinp = chemtools.calculators.gamessus:writeorbinp',
             'bsconvert = chemtools.basisset:bsconvert',
             'bsprint = chemtools.basisset:bsprint',
         ]
     },
-    license = open('LICENSE.rst').read(),
-    long_description = readme(),
-    name = 'chemtools',
-    packages = ['chemtools', 'chemtools/pescan', 'chemtools/calculators'],
-    scripts = [
+    license=open('LICENSE.rst').read(),
+    long_description=readme(),
+    name='chemtools',
+    packages=['chemtools', 'chemtools/pescan', 'chemtools/calculators'],
+    scripts=[
         'chemtools/submitgamess.py',
         'chemtools/submitmolpro.py',
-        'scripts/bsold2new',
-               ],
-    url = 'https://bitbucket.org/lukaszmentel/chemtools/',
-    version = '0.8.1',
-    classifiers = [
+        'scripts/bsold2new'],
+    url='https://bitbucket.org/lukaszmentel/chemtools/',
+    version='0.8.1',
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'License :: MIT',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2.7',
         'Topic :: Scientific/Engineering :: Chemistry',
-        'Topic :: Scientific/Engineering :: Physics',
-    ],
-    keywords = ['basis set', 'optimization', 'quantum chemistry', 'molecular physics'],
+        'Topic :: Scientific/Engineering :: Physics'],
+    keywords=['basis set', 'optimization', 'quantum chemistry',
+              'molecular physics'],
     tests_require=['tox', 'pytest'],
-    cmdclass = {'test': Tox},
+    cmdclass={'test': Tox},
 )
