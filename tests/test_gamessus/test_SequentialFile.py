d1bfc3d167547394b60522156aa7349dd474e469
Lukasz Mentel <lmmentel@gmail.com>
2017-02-24 14:14:29 +0100

    pep8-ing

--- tests/test_gamessus/test_SequentialFile.py
+++ tests/test_gamessus/test_SequentialFile.py
@@ -4,10 +4,12 @@ from chemtools.calculators.gamessreader import SequentialFile
 import os
 import numpy as np
 
+
 class TestSequentialFileHeMini(unittest.TestCase):
 
     def setUp(self):
-        seqfile = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/he/he_mini_hf.F08')
+        seqfile = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                               'data/he/he_mini_hf.F08')
         self.seq = SequentialFile(seqfile)
 
     def tearDown(self):
@@ -15,25 +17,30 @@ class TestSequentialFileHeMini(unittest.TestCase):
 
     def test_read_irect_he(self):
         ints = np.array([0.12856711538353], dtype=float)
-        self.assertAlmostEqual(self.seq.readseq(), ints)
+        self.assertTrue(np.allclose(self.seq.readseq(), ints))
+
 
 class TestSequentialFileNeDZAO(unittest.TestCase):
 
     def setUp(self):
-        seqfile = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/ne/ne_dz_guga.F08')
+        seqfile = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                               'data/ne/ne_dz_guga.F08')
         self.seq = SequentialFile(seqfile)
 
     def tearDown(self):
         self.seq.file.close()
 
     def test_read_ao_integrals_ne(self):
-        ints = np.load(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/ne/ne_dz_guga_aoints.npy'))
+        ints = np.load(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                    'data/ne/ne_dz_guga_aoints.npy'))
         self.assertTrue(np.allclose(self.seq.readseq(), ints))
+
 
 class TestSequentialFileNeDZMO(unittest.TestCase):
 
     def setUp(self):
-        seqfile = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/ne/ne_dz_guga.F09')
+        seqfile = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                               'data/ne/ne_dz_guga.F09')
         self.seq = SequentialFile(seqfile)
 
     def tearDown(self):
@@ -45,13 +52,16 @@ class TestSequentialFileNeDZMO(unittest.TestCase):
         pass
 
     def test_read_mo_two_electron_integrals_ne(self):
-        ints = np.load(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/ne/ne_dz_guga_moints.npy'))
+        ints = np.load(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                    'data/ne/ne_dz_guga_moints.npy'))
         self.assertTrue(np.allclose(self.seq.readseq(mos=True, skip_first=True), ints))
+
 
 class TestSequentialFileNeDZ2RDM(unittest.TestCase):
 
     def setUp(self):
-        seqfile = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/ne/ne_dz_guga.F15')
+        seqfile = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                               'data/ne/ne_dz_guga.F15')
         self.seq = SequentialFile(seqfile)
 
     def tearDown(self):
@@ -60,6 +70,7 @@ class TestSequentialFileNeDZ2RDM(unittest.TestCase):
     def test_read_two_particle_density_matrix_ne(self):
         ints = np.load(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/ne/ne_dz_guga_rdm2.npy'))
         self.assertTrue(np.allclose(self.seq.readseq(mos=True, skip_first=False), ints))
+
 
 if __name__ == "__main__":
     unittest.main()
